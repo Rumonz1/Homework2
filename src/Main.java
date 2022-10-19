2b8b98904a3c4313111f7c786807f781e915cebd
Rumonz <lone_assassin@mail.ru>
2022-10-20 01:34:31 +0300

    Инициализировал переменную friend и выполнил 3 действия с данной переменной

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,11 @@ public class Main {
         paper = paper + 4;
         paper = paper -7639;
         System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend +2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
     }
 }
